c89d5dafebe584f230074ac6e28190c4088cb546
Hitarth Desai <83507760+hitarthdesai@users.noreply.github.com>
2024-07-30 10:02:09 +0530

    cleanup

--- firebase.js
+++ firebase.js
@@ -4,12 +4,12 @@ import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB2Eaw65CFf9LKMPio3vU_BhSm6sWKPJf4",
-  authDomain: "gaming-linkedin.firebaseapp.com",
-  projectId: "gaming-linkedin",
-  storageBucket: "gaming-linkedin.appspot.com",
-  messagingSenderId: "207295354015",
-  appId: "1:207295354015:web:e7264ec900b9d28f693eaa"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
